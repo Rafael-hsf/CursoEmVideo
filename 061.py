13f053e6dfd99fd5d0919828df6af1e62e5fd21b
rafael <rafael.hsf98@gmail.com>
2025-07-15 22:15:34 -0300

    commit 04/06

--- 061.py
+++ 061.py
@@ -1,2 +1,13 @@
 #REFAÇA O EXERCICIO 51 LENDO O PRIMEIRO TERMO E A RAZÃO DE UMA PROGRSSÃO ARITIMETICA, MOSTRANDO OS 10 PRIMEIROS TERMOS
 #DA PROGRESSÃO USANDO A ESTRUTURA WHILE
+
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+contador = 1
+print('Os 10 primeiros termos da PA são:')
+
+while contador < 11:
+    print('{}'.format(primeiro), end = ' → ')
+    primeiro += razão
+    contador += 1
+print('FIM!!')
