799d52d33cbfbba5b08a1cb55bdfcd9d1f9b4697
rafael <rafael.hsf98@gmail.com>
2025-09-03 17:59:24 -0300

    commit 04/06

--- 072.py
+++ 072.py
@@ -1,3 +1,17 @@
 '''  Crie um programa que tenha um tupla totalmente preenchida com uma contagem por extenso de zero até vinte.
 Seu programa deverá ler um numero pelo teclado (entre 0 e 20) e mostra-lo por extenso.
 '''
+
+contagem = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenoveze', 'vinte')
+while True:
+    numero = int(input('Escolha um número entre 0 e 20: '))
+    if numero in (0, 20):
+        print(f'Você escolheu o numero {numero}, {contagem[numero].upper()}')
+        break
+    else:
+        print('Opção invalida, por favor insira um numero entre 0 e 20')
+
+
+
+
+
--- 073.py
+++ 073.py
@@ -2,5 +2,12 @@
 a) apenas os 5 primeiros colocados
 b) os ultimos 4 colocados da tabela
 c) uma lista com os times em ordem alfabética
-d) Em que posição da tabela esta o time da chapecoense
+d) Em que posição da tabela esta o time do São Paulo
 '''
+
+brasileiro = ('Flamengo', 'Cruzeiro', 'Palmeiras', 'Bahia', 'Botafogo', 'Mirassol', 'São Paulo', 'Bragantino', 'Fluminense', 'Internacional', 'Ceara', 'Corinthians', 'Grêmio', 'Atletico MG', 'Vasco', 'Santos', 'Vitoria', 'Juventude', 'Fortaleza', 'Sport')
+print(f'Os 5 primeiros colocados são: {brasileiro[:5]}')
+print(f'Os 4 ultimos colocados são: {brasileiro[-4:]}')
+print(f'Em ordem alfabética: {sorted(brasileiro)}')
+posição = brasileiro.index('São Paulo')
+print(f'O São Paulo esta na posição: {posição + 1}')
